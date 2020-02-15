5992efec9a6de29f14dc8c0e97db7e8f99b67959
Ed <ed@Eds-MacBook-Air.lan>
2020-02-15 20:59:03 +0000

    search items styled, added changes for each journey

--- frontend/components/Search/JourneyCard.js
+++ frontend/components/Search/JourneyCard.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
 
 import './JourneyCard.css';
 
 class JourneyCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showChanges: false
+        }
+        this.handleChangesClick = this.handleChangesClick.bind(this);
+    }
+
+    handleChangesClick(e) {
+        this.setState({
+            showChanges: !this.state.showChanges
+        });
+    }
+
     render() {
-        return (
-            <div className="JourneyCard-container">
-                <h2>Departure: Coventry</h2>
-                <h2>Destination: London</h2>
-                <p>Timings: </p>
-                <p>Price: £50</p>
-                <p>CO2 Footprint: 200</p>
-                <button>Book</button>
+        let changes
+        if (this.state.showChanges === false) {
+            changes
+        } else {
+            changes = <div className="JourneyCard-changes-container">
+                <p>Changes for the journey</p>
+                <ul>
+                    <li>Departure: London | 18:00</li>
+                    <KeyboardArrowDown />
+                    <li>blah</li>
+                    <KeyboardArrowDown />
+                    <li>blah</li>
+                    <KeyboardArrowDown />
+                    <li>blah</li>
+                    <KeyboardArrowDown />
+                    <li>blah</li>
+                    <KeyboardArrowDown />
+                    <li>blah</li>
+                </ul>
             </div>
+        }
+
+        return (
+
+            <div className="JourneyCard-container">
+                <div>
+                    <div className="JourneyCard-info-container">
+                        <p>15:00 -> 17:00</p>
+                        <button onClick={this.handleChangesClick}>Changes: 4</button>
+                        <p>Price: £50</p>
+                        <p>CO2 Footprint: 200</p>
+                        <button>Book</button>
+                    </div>
+                    {changes}
+                </div>
+            </div>
+
         );
     }
 }
--- frontend/components/Search/Search.js
+++ frontend/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import JourneyCard from './JourneyCard';
+import Fade from 'react-reveal';
 
 import './Search.css';
 
@@ -11,13 +12,35 @@ class Search extends Component {
     render() {
         return (
             <div className="Search-container">
-                <JourneyCard />
-                <JourneyCard />
-                <JourneyCard />
-                <JourneyCard />
-                <JourneyCard />
-                <JourneyCard />
-            </div>
+                <Fade right>
+                    <div className="Search-sidebar">
+                        <br />
+                        <h2>Review your order</h2>
+                        <h3>Out 15th Feb 20:00</h3>
+                        <p>Departure: Coventry</p>
+                        <p>Arrival: London</p>
+                        <button>Change</button>
+                        <br />
+                        <h3>Return 17th Feb 17:00</h3>
+                        <p>Departure: Coventry</p>
+                        <p>Arrival: London</p>
+                        <button>Change</button>
+                    </div>
+                </Fade>
+                <div className="Search-cards-container">
+
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                    <JourneyCard />
+                </div>
+            </div >
         );
     }
 }
